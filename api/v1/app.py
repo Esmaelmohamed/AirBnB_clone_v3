df7d765525e0701c9546e2d5c39e12e17d540c0f
Elab <esmael@kali>
2024-05-27 19:27:07 +0300

    new

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,30 +1,38 @@
 #!/usr/bin/python3
-"""Flask application setup"""
-
+"""app"""
 from flask import Flask, make_response, jsonify
 from models import storage
 from api.v1.views import app_views
 from os import getenv
 from flask_cors import CORS
 
+
 app = Flask(__name__)
-CORS(app, resources={r"/api/*": {"origins": "0.0.0.0"}})
+cors = CORS(app, resources={r"/api/*": {"origins": "0.0.0.0"}})
+
 
 app.url_map.strict_slashes = False
 app.register_blueprint(app_views)
 
+
 @app.teardown_appcontext
-def teardown_db(exception):
-    '''Closes the storage engine on teardown'''
+def tear(self):
+    ''' closes storage engine '''
     storage.close()
+
 
 @app.errorhandler(404)
 def not_found(error):
-    '''Handles 404 errors and returns a JSON response'''
+    ''' handles 404 error and gives json formatted response '''
     return make_response(jsonify({'error': 'Not found'}), 404)
 
 if __name__ == '__main__':
-    # Setting host and port for the Flask application
-    host = getenv("HBNB_API_HOST", '0.0.0.0')
-    port = int(getenv("HBNB_API_PORT", 5000))
-    app.run(host=host, port=port, threaded=True)
+    if getenv("HBNB_API_HOST") is None:
+        HBNB_API_HOST = '0.0.0.0'
+    else:
+        HBNB_API_HOST = getenv("HBNB_API_HOST")
+    if getenv("HBNB_API_PORT") is None:
+        HBNB_API_PORT = 5000
+    else:
+        HBNB_API_PORT = int(getenv("HBNB_API_PORT"))
+    app.run(host=HBNB_API_HOST, port=HBNB_API_PORT, threaded=True)
